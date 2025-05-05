ad4111e569e47a2585c933000205aaf29ada1dd5
saikamal@45 <saikamal2727@gmail.com>
2025-05-05 16:46:37 +0530

    UpdatedAgents

--- src/app/dashboards/agentvisitlist/agentvisitlist.component.ts
+++ src/app/dashboards/agentvisitlist/agentvisitlist.component.ts
@@ -3,16 +3,75 @@ import { AgenttopbarComponent } from '../agenttopbar/agenttopbar.component';
 import { AgentDataComponent } from '@views/agents/list/components/agent-data/agent-data.component';
 import { AgentdashboardComponent } from '../agentdashboardsidebar/agentdashboard.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-agentvisitlist',
   standalone: true,
-  imports: [AgenttopbarComponent,AgentdashboardComponent,CommonModule],
+  imports: [AgenttopbarComponent,AgentdashboardComponent,CommonModule,FormsModule],
   templateUrl: './agentvisitlist.component.html',
   styleUrl: './agentvisitlist.component.scss'
 })
 export class AgentvisitlistComponent {
+
+  selectedVisitIndex: number | null = null;
+  selectedStatus: string = '';
+
+
+onActionClick(index: number) {
+  this.selectedVisitIndex = index;
+  this.selectedStatus = this.visits[index].status || '';
+
+}
+
+submitStatus(index: number) {
+  if (this.selectedStatus) {
+    this.visits[index].status = this.selectedStatus;
+    this.selectedVisitIndex = null;
+    this.selectedStatus = '';
+  }
+}
+
+closePropertyModal() {
+  this.selectedVisitIndex = null;
+}
   visits = [
+    {
+      name: 'John Doe',
+      property: 'Sunshine Apartments',
+      date: '2025-04-25',
+      time: '10:00 AM',
+      feedback: 'Interested, asked for price negotiation.',
+      status:''
+    },
+    {
+      name: 'Jane Smith',
+      property: 'Green Villa',
+      date: '2025-04-27',
+      time: '02:30 PM',
+      feedback: 'Requested a second visit with family.'
+    },
+    {
+      name: 'Michael Johnson',
+      property: 'Oceanview Residence',
+      date: '2025-04-28',
+      time: '11:15 AM',
+      feedback: 'Did not show up for the visit.'
+    },
+    {
+      name: 'Emily Brown',
+      property: 'Hilltop House',
+      date: '2025-04-29',
+      time: '09:00 AM',
+      feedback: 'Loved the place, planning to proceed.'
+    },
+    {
+      name: 'David Wilson',
+      property: 'Palm Grove Villa',
+      date: '2025-04-30',
+      time: '04:00 PM',
+      feedback: 'Looking for similar options nearby.'
+    },
     {
       name: 'John Doe',
       property: 'Sunshine Apartments',
