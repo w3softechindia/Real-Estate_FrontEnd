819fd91d1c780a9c5a4cb887691d41a00ebecc81
Sri-Charan-0516 <sricharan824@gmail.com>
2025-05-07 08:17:44 +0530

    07/05/2025-charan

--- src/app/modals/user.model.ts
+++ src/app/modals/user.model.ts
@@ -1,58 +1,85 @@
 export interface Role {
-    roleName: string;
-  }
-  
-  export interface RealEStateUser {
-    id: number;
-    email: string;
-    password: string;
-    phoneNumber: string;
-    roles: Role[];
-  }
-  
-  // ==================== ADMIN ====================
-  export interface Admin extends RealEStateUser {
-    adminName: string;
-  }
-  
-  // ==================== AGENCY ====================
-  export interface Agency extends RealEStateUser {
-    agencyName: string;
-    agencyAddress: string;
-    agencyPinCode: number;
-    city: string;
-    state: string;
-    fbUrl: string;
-    instagramUrl: string;
-    twitterUrl: string;
-    registrationDate: string;
-    status:string;
-    agents: Agent[];
-  }
-  
-  // ==================== AGENT ====================
-  export interface Agent extends RealEStateUser {
-    agentName: string;
-    address: string;
-    pincode: number;
-    city: string;
-    state: string;
-    agency: Agency;
-    registrationDate: string;
-    status:string;
-    customers: Customer[];
-  }
-  
-  // ==================== CUSTOMER ====================
-  export interface Customer extends RealEStateUser {
-    customerName: string;
-    address: string;
-    pincode: number;
-    city: string;
-    state: string;
-    agent: Agent;
-    registrationDate: string;
-    status:string;
-    agencyName: string;
-  }
-  
+  roleName: string;
+}
+
+export interface RealEStateUser {
+  id: number;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  roles: Role[];
+}
+
+// ==================== ADMIN ====================
+export interface Admin extends RealEStateUser {
+  adminName: string;
+}
+
+// ==================== AGENCY ====================
+export interface Agency extends RealEStateUser {
+  agencyName: string;
+  agencyAddress: string;
+  agencyPinCode: number;
+  city: string;
+  state: string;
+  fbUrl: string;
+  instagramUrl: string;
+  twitterUrl: string;
+  registrationDate: string;
+  status: string;
+  agents: Agent[];
+}
+
+// ==================== AGENT ====================
+export interface Agent extends RealEStateUser {
+  agentName: string;
+  address: string;
+  pincode: number;
+  city: string;
+  state: string;
+  agency: Agency;
+  registrationDate: string;
+  status: string;
+  customers: Customer[];
+}
+
+// ==================== CUSTOMER ====================
+export interface Customer extends RealEStateUser {
+  customerName: string;
+  address: string;
+  pincode: number;
+  city: string;
+  state: string;
+  agent: Agent;
+  registrationDate: string;
+  status: string;
+  agencyName: string;
+}
+
+//======================Venture========================
+export interface Venture {
+  ventureId?: number;
+  ventureName: string;
+  ventureSize: string;
+  totalPlots: number;
+  availablePlots: number;
+  bookedPlots: number;
+  soldPlots: number;
+  address: string;
+  city: string;
+  state: string;
+  phno: number;
+  pincode: number;
+  plots: Plots[];
+}
+
+//=======================Plots============================
+export interface Plots {
+  plotNumber: string;
+  plotSize: number;
+  price: number;
+  status: 'AVAILABLE' | 'BOOKED' | 'SOLD'; // Enum-like
+  location: string;
+  facing: string;
+  cornerPlot: boolean;
+}
--- src/app/views/property/list/components/property-data/property-data.component.ts
+++ src/app/views/property/list/components/property-data/property-data.component.ts
@@ -1,5 +1,7 @@
+import { Venture } from '@/app/modals/user.model'
+import { RealEStateService } from '@/app/services/real-estate.service'
 import { CommonModule, DecimalPipe } from '@angular/common'
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core'
 import { currency } from '@common/constants'
 import {
   NgbDropdownModule,
@@ -10,11 +12,24 @@ import { propertyData } from '@views/property/data'
 @Component({
   selector: 'property-data',
   standalone: true,
-  imports: [DecimalPipe, CommonModule, NgbPaginationModule, NgbDropdownModule],
+  imports: [CommonModule, NgbPaginationModule, NgbDropdownModule],
   templateUrl: './property-data.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class PropertyDataComponent {
-  propertyList = propertyData
-  currency = currency
+export class PropertyDataComponent{
+  // propertyList = propertyData
+  // currency = currency
+  ventures:Venture[]=[];
+
+  constructor(private service:RealEStateService){}
+
+  ngOnInit(){
+    this.getAllVentures();
+  }
+
+  private getAllVentures(){
+    this.service.getAllVentures().subscribe((data:any[])=>{
+      this.ventures=data;
+    })
+  }
 }
