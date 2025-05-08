17b40d5abcc7032365b1cdf32be3f47a59b0ac9e
SaiKamal <141002780+Saikamal45@users.noreply.github.com>
2025-05-08 16:51:13 +0530

    Kamal (#23)
    * UpdatedAgents
    
    * LeadListUpdated

--- src/app/dashboards/agentleadslist/agentleadslist.component.ts
+++ src/app/dashboards/agentleadslist/agentleadslist.component.ts
@@ -2,144 +2,63 @@ import { Component } from '@angular/core';
 import { AgenttopbarComponent } from '../agenttopbar/agenttopbar.component';
 import { AgentdashboardComponent } from '../agentdashboardsidebar/agentdashboard.component';
 import { CommonModule } from '@angular/common';
+import { Lead } from '@/app/modals/user.model';
+import { RealEStateService } from '@/app/services/real-estate.service';
+import { subscribeOn } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-agentleadslist',
   standalone: true,
 
-  imports: [AgenttopbarComponent,AgentdashboardComponent,CommonModule],
+  imports: [AgenttopbarComponent,AgentdashboardComponent,CommonModule,FormsModule],
 
   templateUrl: './agentleadslist.component.html',
   styleUrl: './agentleadslist.component.scss'
 })
 export class AgentleadslistComponent {
-leads=[
-  {
-    name:'sai',
-    location:'Ecil',
-    city:'Hyderabad',
-    state:"Telangana",
-    pincode:500062,
-    area:'kushaiguda',
-    type:'villa',
-    status:'Available',
-    phoneNumber:'9603476942'
-  },
-  {
-    name:'charan',
-    location:'SangaReddy',
-    city:'Hyderabad',
-    state:"Telangana",
-    pincode:'000000',
-    area:'kushaiguda',
-    type:'villa',
-    status:'Available',
-    phoneNumber:'8919483290'
-  },
-  
-  {
-    name:'Poorna',
-    location:'SangaReddy',
-    city:'Hyderabad',
-    state:"Telangana",
-    pincode:'000000',
-    area:'kushaiguda',
-    type:'villa',
-    status:'Available',
-    phoneNumber:'8919483290'
-  },
 
-  {
-    name:'charan',
-    location:'SangaReddy',
-    city:'Hyderabad',
-    state:"Telangana",
-    pincode:'000000',
-    area:'kushaiguda',
-    type:'villa',
-    status:'Available',
-    phoneNumber:'8919483290'
-  },
-  {
-    name:'sai',
-    location:'Ecil',
-    city:'Hyderabad',
-    state:"Telangana",
-    pincode:500062,
-    area:'kushaiguda',
-    type:'villa',
-    status:'Available',
-    phoneNumber:'9603476942'
-  },
-  {
-    name:'charan',
-    location:'SangaReddy',
-    city:'Hyderabad',
-    state:"Telangana",
-    pincode:'000000',
-    area:'kushaiguda',
-    type:'villa',
-    status:'Available',
-    phoneNumber:'8919483290'
-  },
-  
-  {
-    name:'Poorna',
-    location:'SangaReddy',
-    city:'Hyderabad',
-    state:"Telangana",
-    pincode:'000000',
-    area:'kushaiguda',
-    type:'villa',
-    status:'Available',
-    phoneNumber:'8919483290'
-  },
+leads:Lead[]=[];
+showModal: boolean = false;
+selectedLead: any = {};
 
-  {
-    name:'charan',
-    location:'SangaReddy',
-    city:'Hyderabad',
-    state:"Telangana",
-    pincode:'000000',
-    area:'kushaiguda',
-    type:'villa',
-    status:'Available',
-    phoneNumber:'8919483290'
-  },
-  {
-    name:'charan',
-    location:'SangaReddy',
-    city:'Hyderabad',
-    state:"Telangana",
-    pincode:'000000',
-    area:'kushaiguda',
-    type:'villa',
-    status:'Available',
-    phoneNumber:'8919483290'
-  },
-  
-  {
-    name:'Poorna',
-    location:'SangaReddy',
-    city:'Hyderabad',
-    state:"Telangana",
-    pincode:'000000',
-    area:'kushaiguda',
-    type:'villa',
-    status:'Available',
-    phoneNumber:'8919483290'
-  },
+constructor(private service:RealEStateService){}
 
-  {
-    name:'charan',
-    location:'SangaReddy',
-    city:'Hyderabad',
-    state:"Telangana",
-    pincode:'000000',
-    area:'kushaiguda',
-    type:'villa',
-    status:'Available',
-    phoneNumber:'8919483290'
+ngOnInit():void{
+this.getAllLeads();
+}
+
+getAllLeads():void{
+this.service.getAllLeads().subscribe({
+  next:(res)=>{
+    this.leads=res;
+  },error:(err)=>{
+    console.error('Failed to fetch leads:', err);
   }
-];
+})
+}
+
+openUpdateModal(lead: any) {
+  console.log("Opening modal with agent data:", lead);
+  this.selectedLead = { ...lead };
+  this.showModal = true;
+}
+
+closeModal() {
+  this.showModal = false;
+  console.log("Modal closed");
+}
+
+deleteLead(lead: any) {
+  this.leads = this.leads.filter(a => a.email !== lead.emaill);
+}
+
+// updateLead() {
+//   console.log("Updating agent:", this.selectedLead);
+//   const index = this.leads.findIndex(lead => lead.email === this.selectedLead.emaill);
+//   if (index !== -1) {
+//     this.leads[index] = { ...this.selectedLead };
+//   }
+//   this.closeModal();
+// }
 }
