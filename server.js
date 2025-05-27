061b7e543487cb430e26486e90bc8958fd4be556
Sri-Charan-0516 <sricharan824@gmail.com>
2025-05-27 15:47:57 +0530

    27/05/2025-charan

---
+++ server.js
@@ -0,0 +1,11 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.static(__dirname+'/dist/lahomes'));
+
+app.get('/*',(req,resp)=>{
+    resp.sendFile(__dirname+'/dist/lahomes/index.html');
+});
+
+app.listen(process.env.PORT || 4200);
