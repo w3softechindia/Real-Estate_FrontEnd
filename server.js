3cba1cb0468c102b60cc972b4da6fa77c0f77d62
Sri-Charan-0516 <sricharan824@gmail.com>
2025-05-30 10:24:42 +0530

    server.js changed

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ let express = require('express');
 
 let app = express();
 
-app.use(express.static(__dirname+'/dist/lahomes'));
+app.use(express.static(__dirname+'/dist/lahomes/browser'));
 
 app.get('/*',(req,resp)=>{
-    resp.sendFile(__dirname+'/dist/lahomes/index.html');
+    resp.sendFile(__dirname+'/dist/lahomes/browser/index.html');
 });
 
 app.listen(process.env.PORT || 4200);
