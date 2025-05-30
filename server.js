a22b2f8f6c6649632111e30bf19ecb2aefa98e9c
Sri-Charan-0516 <sricharan824@gmail.com>
2025-05-30 08:39:41 +0530

    30/05/2025

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const path = require('path');
 
 const app = express();
 
-// Serve static Angular files
+// Serve Angular static files
 app.use(express.static(path.join(__dirname, 'dist/lahomes')));
 
-// Wildcard route should return index.html for SPA routing
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/lahomes', 'index.html'));
+// Correct wildcard route pattern
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/lahomes/index.html'));
 });
 
-// Use Heroku port or 4200 locally
 const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
